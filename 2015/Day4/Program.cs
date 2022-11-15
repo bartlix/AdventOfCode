05f88afa735642ce26666aadc39a3f73bbbdbb4a
Thomas Resch <thomas.resch@kcc.at>
2022-11-15 19:37:32 +0100

    Day 4 - Part 2

--- 2015/Day4/Program.cs
+++ 2015/Day4/Program.cs
@@ -6,21 +6,47 @@ namespace Day2
     {
         [STAThread]
         // Part 1
+        //static void Main(string[] args)
+        //{
+        //    var content = "yzbqklnj";
+
+        //    var result = "";
+
+        //    for (var i = 0; i < 1000000; i++) 
+        //    {
+        //        var tryout = $"{content}{i:D6}";
+
+        //        var bla = CreateMD5(tryout);
+
+        //        if (bla.StartsWith("00000"))
+        //        {
+        //            result = $"{i:D6}";
+        //            break;
+        //        }
+        //    }
+
+        //    Console.WriteLine(result);
+        //    Clipboard.SetText(result);
+
+        //    Console.ReadKey();
+        //}
+
+        //Part 2
         static void Main(string[] args)
         {
             var content = "yzbqklnj";
 
             var result = "";
 
-            for (var i = 0; i < 1000000; i++) 
+            for (var i = 0; i < int.MaxValue; i++)
             {
-                var tryout = $"{content}{i:D6}";
+                var tryout = $"{content}{i}";
 
                 var bla = CreateMD5(tryout);
 
-                if (bla.StartsWith("00000"))
+                if (bla.StartsWith("000000"))
                 {
-                    result = $"{i:D6}";
+                    result = $"{i}";
                     break;
                 }
             }
