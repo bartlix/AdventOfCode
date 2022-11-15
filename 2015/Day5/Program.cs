fa494b070f63e8f3f766d7b3be9156a7e7a0f3d3
Thomas Resch <thomas.resch@kcc.at>
2022-11-15 20:14:32 +0100

    Day 5 - Part 2

--- 2015/Day5/Program.cs
+++ 2015/Day5/Program.cs
@@ -7,38 +7,75 @@ namespace Day2
     {
         [STAThread]
         // Part 1
+        //static void Main(string[] args)
+        //{
+        //    var content = File.ReadAllLines("Input.txt");
+
+        //    var niceCount = 0;
+
+        //    foreach(var line in content)
+        //    {
+        //        if(line.Contains("ab") || line.Contains("cd") || line.Contains("pq") || line.Contains("xy"))
+        //        {
+        //            continue;
+        //        }
+
+        //        var reg = new Regex("[aeiou]");
+        //        var match = reg.Matches(line);
+
+        //        if (match.Count() < 3)
+        //        {
+        //            continue;
+        //        }
+
+        //        var reg2 = new Regex("(\\w)\\1{1}");
+        //        var match2 = reg2.Matches(line);
+
+        //        if (match2.Count() == 0)
+        //        {
+        //            continue;
+        //        }
+
+        //        niceCount++;
+        //    }
+
+
+        //    Console.WriteLine(niceCount);
+        //    Clipboard.SetText(niceCount.ToString());
+
+        //    Console.ReadKey();
+        //}
+
+        //Part 2
         static void Main(string[] args)
         {
             var content = File.ReadAllLines("Input.txt");
 
             var niceCount = 0;
 
-            foreach(var line in content)
+            foreach (var line in content)
             {
-                if(line.Contains("ab") || line.Contains("cd") || line.Contains("pq") || line.Contains("xy"))
-                {
-                    continue;
-                }
-
-                var reg = new Regex("[aeiou]");
+                var reg = new Regex("([a-zA-Z]{2,}).*?\\1");
                 var match = reg.Matches(line);
 
-                if (match.Count() < 3)
+                if (match.Count() == 0)
                 {
                     continue;
                 }
 
-                var reg2 = new Regex("(\\w)\\1{1}");
-                var match2 = reg2.Matches(line);
-
-                if (match2.Count() == 0)
+                for (var i = 0; i < line.Length - 2; i++)
                 {
-                    continue;
+                    if (line[i] == line[i + 2])
+                    {
+                        niceCount++;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
-
-                niceCount++;
             }
-            
 
             Console.WriteLine(niceCount);
             Clipboard.SetText(niceCount.ToString());
