a9af3b507f7e95be4a62c26adc0c7e4078982550
Thomas Resch <Thomas.Resch@kcc.at>
2022-11-16 13:37:03 +0100

    Day 6 - Part 1

---
+++ 2015/Day6/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Day2
+{
+    class Programm
+    {
+        [STAThread]
+        // Part 1
+        static void Main(string[] args)
+        {
+            var content = File.ReadAllLines("Input.txt");
+
+            Dictionary<(int x, int y), bool> playground = new ();
+
+            for (var x = 0; x < 1000; x++)
+            {
+                for (var y = 0; y < 1000; y++)
+                {
+                    playground.Add((x,y), false);
+                }
+            }
+
+            int rowCount = 0;
+            foreach (var row in content)
+            {
+                Console.WriteLine(rowCount++);
+                ApplyAction(row, playground);
+            }
+
+            var count = playground.Where(x => x.Value).Count();
+            Console.WriteLine($"Result: {count}");
+            Clipboard.SetText(count.ToString());
+
+            Console.ReadKey();
+        }
+
+        private static void ApplyAction(string row, Dictionary<(int x,int y), bool> playground)
+        {
+            LightAction action = LightAction.None;
+
+            if (row.StartsWith("turn on"))
+            {
+                action = LightAction.On;
+                row = row.Replace("turn on ", "");
+            }
+
+            if (row.StartsWith("turn off"))
+            {
+                action = LightAction.Off;
+                row = row.Replace("turn off ", "");
+            }
+
+            if (row.StartsWith("toggle"))
+            {
+                action = LightAction.Toggle;
+                row = row.Replace("toggle ", "");
+            }
+
+            var startPoint = row.Split(' ')[0].Split(',');
+            var startX = int.Parse(startPoint[0]);
+            var startY = int.Parse(startPoint[1]);
+
+            row = row.Substring(row.IndexOf(' ') + 1);
+
+            row = row.Replace("through ", "");
+
+            var endPoint = row.Split(',');
+            var endX = int.Parse(endPoint[0]);
+            var endY = int.Parse(endPoint[1]);
+
+            for (var x = startX; x <= endX; x++)
+            {
+                for (var y = startY; y <= endY; y++)
+                {
+                    switch (action)
+                    {
+                        case LightAction.On:
+                            playground[(x,y)] = true;
+                            break;
+                        case LightAction.Off:
+                            playground[(x, y)] = false;
+                            break;
+                        case LightAction.Toggle:
+                            playground[(x, y)] = !playground[(x, y)];
+                            break;
+                    }
+                }
+            }
+        }
+
+        private enum LightAction
+        {
+            None = 0,
+            On = 1,
+            Off = 2,
+            Toggle = 3,
+        }
+    }
+}
