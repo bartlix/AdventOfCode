42f2a1678afaed60cb245f5743fcd3ff6f4aa100
Thomas Resch <thomas.resch@kcc.at>
2022-11-17 20:43:08 +0100

    Day 8 - Part 2

--- 2015/Day8/Program.cs
+++ 2015/Day8/Program.cs
@@ -3,12 +3,43 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 
-namespace Day2
+namespace Day8
 {
     class Programm
     {
         [STAThread]
         //Part 1
+        //static void Main(string[] args)
+        //{
+        //    var input = File.ReadAllLines("Input.txt");
+        //    //var input = File.ReadAllLines("Sample.txt");
+        //    var sum = 0;
+
+        //    foreach (var row in input)
+        //    {
+        //        var str = GetString(row);
+
+        //        var zahl = row.Length - str.Length;
+
+        //        sum += zahl;
+        //    }
+
+        //    Console.WriteLine($"Result: {sum}");
+        //    Clipboard.SetText(sum.ToString());
+
+        //    Console.ReadKey();
+        //}
+
+        //private static string GetString(string input)
+        //{
+        //    var s = input.Substring(1, input.Length - 2);
+
+        //    s = Regex.Unescape(s);
+
+        //    return s;
+        //}
+
+        // Part 2
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("Input.txt");
@@ -19,7 +50,7 @@ namespace Day2
             {
                 var str = GetString(row);
 
-                var zahl = row.Length - str.Length;
+                var zahl = str.Length - row.Length;
 
                 sum += zahl;
             }
@@ -32,11 +63,30 @@ namespace Day2
 
         private static string GetString(string input)
         {
-            var s = input.Substring(1, input.Length - 2);
+            StringBuilder sb = new StringBuilder();
 
-            s = Regex.Unescape(s);
+            sb.Append('\"');
 
-            return s;
+            foreach(var c in input)
+            {
+                if(c == '\"')
+                {
+                    sb.Append('\\');
+
+                }
+
+                if(c == '\\')
+                {
+                    sb.Append('\\');
+                }
+
+
+                sb.Append(c);
+            }
+
+            sb.Append('\"');
+
+            return sb.ToString();
         }
     }
 }
