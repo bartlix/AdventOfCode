4bf5bde033213ab5bfcf9fca8b9148f4c36234f3
Thomas Resch <Thomas.Resch@kcc.at>
2022-11-17 09:16:11 +0100

    Day 7 - Part 1

--- 2015/Day7/Program.cs
+++ 2015/Day7/Program.cs
@@ -6,112 +6,168 @@ namespace Day2
 {
     class Programm
     {
+        public static Dictionary<string, ushort> Circuits = new();
+
         [STAThread]
         //Part 1
         static void Main(string[] args)
         {
-            var content = File.ReadAllLines("Input.txt");
-            //var content = File.ReadAllLines("Sample.txt");
+            var input = File.ReadAllLines("Input.txt");
+            //var input = File.ReadAllLines("Sample.txt");
 
-            Dictionary<string, ushort> circuits = new();
+            var basis = new Dictionary<string, bool>(input.Select(x => new KeyValuePair<string, bool>(x, false)));
 
-            foreach (var circuit in content)
+            while (!basis.All(x => x.Value))
             {
-                if(circuit == "0 -> c")
+                Console.WriteLine($"Anzahl: {basis.Count(x => x.Value)}/{basis.Count()}");
+                foreach (var circuit in basis.Where(x => !x.Value).Select(x => x.Key))
                 {
+                    var parts = circuit.Split("->");
 
-                }
+                    var left = parts[0].Trim().Split();
+                    var target = parts[1].Trim();
 
-                var parts = circuit.Split("->");
-
-                var left = parts[0].Trim().Split();
-                var target = parts[1].Trim();
-
-                CheckCircuit(circuits, target);
-
-                if (left.Count() == 1)
-                {
-                    if (ushort.TryParse(left[0], out var result))
+                    if (left.Count() == 1)
                     {
-                        circuits[target] = result;
-                    }
-                    else
-                    {
-                        circuits[target] = circuits[left[0].Trim()];
-                    }
-                }
-
-                if (left.Contains("AND") || left.Contains("OR") || left.Contains("LSHIFT")  || left.Contains("RSHIFT"))
-                {
-                    var a = left[0].Trim();
-                    var b = left[2].Trim();
-
-                    var isNumA = ushort.TryParse(a, out var numA);
-                    var isNumB = ushort.TryParse(b, out var numB);
-
-                    if (!isNumA)
-                    {
-                        CheckCircuit(circuits, a);
+                        if (ushort.TryParse(left[0], out var result))
+                        {
+                            if (SetValue(target, result))
+                            {
+                                basis[circuit] = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (SetValueFromVariable(target, left[0].Trim()))
+                            {
+                                basis[circuit] = true;
+                                continue;
+                            }
+                        }
                     }
 
-                    if (!isNumB)
+                    if (left.Contains("AND") || left.Contains("OR") || left.Contains("LSHIFT") || left.Contains("RSHIFT"))
                     {
-                        CheckCircuit(circuits, b);
+                        var a = left[0].Trim();
+                        var b = left[2].Trim();
+
+                        var isNumA = ushort.TryParse(a, out var numA);
+                        var isNumB = ushort.TryParse(b, out var numB);
+
+                        if (!isNumA)
+                        {
+                            if (!Circuits.ContainsKey(a))
+                            {
+                                continue;
+                            }
+                        }
+
+                        if (!isNumB)
+                        {
+                            if (!Circuits.ContainsKey(b))
+                            {
+                                continue;
+                            }
+                        }
+
+                        ushort lL = isNumA ? numA : Circuits[a];
+                        ushort rR = isNumB ? numB : Circuits[b];
+
+                        if (left.Contains("AND"))
+                        {
+                            if (SetValue(target, (ushort)(lL & rR)))
+                            {
+                                basis[circuit] = true;
+                                break;
+                            }
+                        }
+
+                        if (left.Contains("OR"))
+                        {
+                            if (SetValue(target, (ushort)(lL | rR)))
+                            {
+                                basis[circuit] = true;
+                                break;
+                            }
+                        }
+
+                        if (left.Contains("LSHIFT"))
+                        {
+                            if (SetValue(target, (ushort)(lL << rR)))
+                            {
+                                basis[circuit] = true;
+                                break;
+                            }
+                        }
+
+                        if (left.Contains("RSHIFT"))
+                        {
+                            if (SetValue(target, (ushort)(lL >> rR)))
+                            {
+                                basis[circuit] = true;
+                                break;
+                            }
+                        }
                     }
 
-                    ushort lL = isNumA ? numA : circuits[a];
-                    ushort rR = isNumB ? numB : circuits[b];
-
-                    if (left.Contains("AND"))
+                    if (left.Contains("NOT"))
                     {
-                        circuits[target] = (ushort)(lL & rR);
+                        var a = left[1].Trim();
+
+                        var isNumA = ushort.TryParse(a, out var numA);
+
+                        if (isNumA)
+                        {
+                            if (SetValue(target, (ushort)(~numA)))
+                            {
+                                basis[circuit] = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (Circuits.ContainsKey(a))
+                            {
+                                if (SetValue(target, (ushort)~Circuits[a]))
+                                {
+                                    basis[circuit] = true;
+                                    break;
+                                }
+                            }
+                        }
                     }
-
-                    if (left.Contains("OR"))
-                    {
-                        circuits[target] = (ushort)(lL | rR);
-                    }
-
-                    if (left.Contains("LSHIFT"))
-                    {
-                        circuits[target] = (ushort)(lL << rR);
-                    }
-
-                    if (left.Contains("RSHIFT"))
-                    {
-                        circuits[target] = (ushort)(lL >> rR);
-                    }
-                }
-
-                if (left.Contains("NOT"))
-                {
-                    var a = left[1].Trim();
-
-                    var isNumA = ushort.TryParse(a, out var numA);
-
-                    if (!isNumA)
-                    {
-                        CheckCircuit(circuits, a);
-                    }
-
-                    ushort lL = isNumA ? numA : circuits[a];
-
-                    circuits[target] = (ushort)(~lL & ushort.MaxValue);
                 }
             }
 
-            Console.WriteLine($"Result: {circuits["a"]}");
-            Clipboard.SetText(circuits["a"].ToString());
+            Console.WriteLine($"Result: {Circuits["a"]}");
+            Clipboard.SetText(Circuits["a"].ToString());
 
             Console.ReadKey();
         }
 
-        private static void CheckCircuit(Dictionary<string, ushort> circuits, string target)
+        private static bool SetValue(string target, ushort result)
         {
-            if (!circuits.ContainsKey(target))
+            if (!Circuits.ContainsKey(target))
             {
-                circuits.Add(target, 0);
+                Circuits.Add(target, result);
             }
+            else
+            {
+                Circuits[target] = result;
+            }
+
+            return true;
+        }
+
+        private static bool SetValueFromVariable(string target, string variable)
+        {
+            if (Circuits.ContainsKey(variable))
+            {
+                return SetValue(target, Circuits[variable]);
+            }
+
+            return false;
         }
     }
 }
