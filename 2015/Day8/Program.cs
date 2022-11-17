98d8f07d7c4a9d649ef22c36de92f1e32f2d316b
Thomas Resch <Thomas.Resch@kcc.at>
2022-11-17 14:25:48 +0100

    Day 8 - Part 1

---
+++ 2015/Day8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Day2
+{
+    class Programm
+    {
+        [STAThread]
+        //Part 1
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+            //var input = File.ReadAllLines("Sample.txt");
+            var sum = 0;
+
+            foreach (var row in input)
+            {
+                var str = GetString(row);
+
+                var zahl = row.Length - str.Length;
+
+                sum += zahl;
+            }
+
+            Console.WriteLine($"Result: {sum}");
+            Clipboard.SetText(sum.ToString());
+
+            Console.ReadKey();
+        }
+
+        private static string GetString(string input)
+        {
+            var s = input.Substring(1, input.Length - 2);
+
+            s = Regex.Unescape(s);
+
+            return s;
+        }
+    }
+}
