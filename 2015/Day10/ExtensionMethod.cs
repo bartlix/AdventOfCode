86bc35e79de8416f46e8a7ab9a7b46e7aab26ea4
Thomas Resch <thomas.resch@kcc.at>
2022-11-20 22:05:35 +0100

    Day10 - Part 1

---
+++ 2015/Day10/ExtensionMethod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class ExtensionMethod
+    {
+        public static string LookAndSay(this string input)
+        {
+            if(input.Length == 1)
+            {
+                return $"1{input}";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            int count = 1;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                char next = (i + 1) < input.Length ? input[i+1] : ' ';
+
+                if(c == next)
+                {
+                    count++;
+                    continue;
+                }              
+                else
+                {
+                    sb.Append($"{count}{c}");
+                    count = 1;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 2015/Day10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Day10
+{
+    class Programm
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var input = "1113222113";
+
+            for (var i = 0; i < 40; i++)
+            {
+                input = input.LookAndSay();
+            }
+
+            Console.WriteLine($"Result: {input.Length}");
+            Clipboard.SetText(input.Length.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
