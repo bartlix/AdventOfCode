7fab1aa34b108a9520772c08636cee2c72778e8f
Thomas Resch <thomas.resch@kcc.at>
2022-11-21 22:21:30 +0100

    Day 12 - Part 1

---
+++ 2015/Day12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("Input.txt");
+
+var regex = new Regex("-?[0-9]{1,}");
+
+var matches = regex.Matches(input);
+
+var sum = 0;
+
+foreach (Match match in matches)
+{
+    sum += int.Parse(match.Value);
+}
+
+Console.WriteLine(sum.ToString());
+Console.ReadKey();
