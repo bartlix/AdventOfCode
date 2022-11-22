0bec5fb2a30dd09980fd13df294871ec4b67abf2
Thomas Resch <Thomas.Resch@kcc.at>
2022-11-22 16:45:50 +0100

    Day 15 - with Sample

---
+++ 2015/Day15/IngredientsInfo.cs
@@ -0,0 +1,61 @@
+﻿namespace Day15
+{
+    internal class IngredientsInfo
+    {
+        public IngredientsInfo(string input)
+        {
+            Parse(input);
+        }
+
+        public string Name { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public int Calories { get; private set; }
+
+        public int Texture { get; private set; }
+
+        public int Flavor { get; private set; }
+
+        public int Durability { get; private set; }
+
+        private void Parse(string input)
+        {
+            var parts = input.Split(':');
+
+            Name = parts[0];
+
+            parts = parts[1].Trim().Split(',');
+
+            foreach (var part in parts)
+            {
+                var keyValue = part.Trim().Split(' ');
+
+                if (keyValue[0] == "capacity")
+                {
+                    Capacity = int.Parse(keyValue[1]);
+                }
+
+                if (keyValue[0] == "durability")
+                {
+                    Durability = int.Parse(keyValue[1]);
+                }
+
+                if (keyValue[0] == "flavor")
+                {
+                    Flavor = int.Parse(keyValue[1]);
+                }
+
+                if (keyValue[0] == "texture")
+                {
+                    Texture = int.Parse(keyValue[1]);
+                }
+
+                if (keyValue[0] == "calories")
+                {
+                    Calories = int.Parse(keyValue[1]);
+                }
+            }
+        }
+    }
+}
---
+++ 2015/Day15/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Day15
+{
+    class Programm
+    {
+        [STAThread]
+        //Part 1
+        static void Main(string[] args)
+        {
+            //var input = File.ReadAllLines("Input.txt");
+            var input = File.ReadAllLines("Sample.txt");
+
+            var ingredients = new List<IngredientsInfo>();
+
+            foreach (var row in input)
+            {
+                ingredients.Add(new IngredientsInfo(row));
+            }
+
+            var dict = new Dictionary<int, int>(Enumerable.Range(0, 100).ToDictionary(x => x, x => 0));
+
+
+            foreach (var d in dict)
+            {
+                dict[d.Key] = (ingredients[0].Capacity * d.Key + ingredients[1].Capacity * (100-d.Key))*
+                            (ingredients[0].Texture * d.Key + ingredients[1].Texture * (100 - d.Key))*
+                            (ingredients[0].Flavor * d.Key + ingredients[1].Flavor * (100 - d.Key))*
+                            (ingredients[0].Durability * d.Key + ingredients[1].Durability * (100 - d.Key));
+            }
+
+            var re = dict[44];
+
+            Console.WriteLine(re);
+            Clipboard.SetText(re.ToString());
+            Console.ReadKey();
+        }
+    }
+}
