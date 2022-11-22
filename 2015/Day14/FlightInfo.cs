42234af987a9eb8fa2b1536ddf5c825abaf697bb
Thomas Resch <Thomas.Resch@kcc.at>
2022-11-22 14:48:31 +0100

    Day 14 - Part 1

---
+++ 2015/Day14/FlightInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    public class FlightInfo
+    {
+        public FlightInfo(string data)
+        {
+            Parse(data);
+        }
+
+        public string Name { get; private set; }
+
+        public int Speed { get; private set; }
+
+        public int Duration { get; private set; }
+
+        public int Sleep { get; private set; }
+
+        public int GetDistance(int duration)
+        {
+            var track = 0;
+
+            var rest = duration;
+
+            while(rest > 0)
+            {
+                var time = Duration < rest ? Duration : rest;
+
+                track += time * Speed;
+
+                rest -= time;
+
+                var pause = Sleep < rest ? Sleep : rest;
+
+                rest -= pause;
+                
+
+            }
+
+            return track;
+        }
+
+        private void Parse(string data)
+        {
+            data = data.Replace("can fly", "").Replace("km/s for", "").Replace("seconds,", "").Replace("but then must rest for","").Replace("seconds.", "");
+            var pars = data.Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+            Name = pars[0];
+            Speed = int.Parse(pars[1]);
+            Duration = int.Parse(pars[2]);
+            Sleep = int.Parse(pars[3]);
+        }
+    }
+}
---
+++ 2015/Day14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Day14;
+
+namespace Day13
+{
+    class Programm
+    {
+        [STAThread]
+        //Part 1
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+            //var input = File.ReadAllLines("Sample.txt");
+
+            var deerList = new List<FlightInfo>();
+
+            var resultList = new Dictionary<string, int>();
+
+
+            foreach(var line in input)
+            {   
+                deerList.Add(new FlightInfo(line));
+            }
+
+            foreach(var deer in deerList)
+            {
+                var r = deer.GetDistance(2503);
+
+                resultList.Add(deer.Name, r);
+            }
+
+            var re = resultList.Max(x => x.Value);
+            Console.WriteLine(re);
+            Clipboard.SetText(re.ToString());
+            Console.ReadKey();
+        }
+    }
+}
