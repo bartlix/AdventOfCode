210c5a329129594a74bedd105afef5d9de8ba0fe
Thomas Resch <Thomas.Resch@kcc.at>
2022-11-30 12:46:38 +0100

    Day17 - Part 1

---
+++ 2015/Day17/ExtensionsMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+    public static class ExtensionsMethods
+    {
+        /// <summary>
+        /// Generates combinations with repetitions
+        /// </summary>
+        /// <typeparam name="T">Type of items to combine.</typeparam>
+        /// <param name="items">Array of items. Will not be modified.</param>
+        /// <param name="length">Length of combinations, equal to <paramref name="items"/> length if null</param>
+        /// <param name="reverse">If changes should begin with array end (high indexes) first</param>
+        /// <returns>Combinations of input items.</returns>
+        public static IEnumerable<T[]> Combinations<T>(this T[] items, int? length = null, bool reverse = true)
+        {
+            var itemsLength = items.Length;
+            var combinationLength = length ?? itemsLength;
+            var indexes = new int[combinationLength];
+
+            var overflow = false;
+            while (!overflow)
+            {
+                var combination = new T[combinationLength];
+                for (var i = 0; i < combinationLength; i++)
+                    combination[i] = items[indexes[reverse ? combinationLength - i - 1 : i]];
+                yield return combination;
+
+                overflow = true;
+                for (var i = 0; i < combinationLength && overflow; i++)
+                {
+                    var index = indexes[i] + 1;
+                    overflow = index == itemsLength;
+                    indexes[i] = overflow ? 0 : index;
+                }
+            }
+        }
+
+        public static IEnumerable<IEnumerable<T>> CombinationsWithoutRepetition<T>(
+            this IEnumerable<T> items,
+            int ofLength)
+        {
+            return (ofLength == 1) ?
+                items.Select(item => new[] { item }) :
+                items.SelectMany((item, i) => items.Skip(i + 1)
+                                                   .CombinationsWithoutRepetition(ofLength - 1)
+                                                   .Select(result => new T[] { item }.Concat(result)));
+        }
+
+        public static IEnumerable<IEnumerable<T>> CombinationsWithoutRepetition<T>(
+            this IEnumerable<T> items,
+            int ofLength,
+            int upToLength)
+        {
+            return Enumerable.Range(ofLength, Math.Max(0, upToLength - ofLength + 1))
+                             .SelectMany(len => items.CombinationsWithoutRepetition(ofLength: len));
+        }
+    }
+}
---
+++ 2015/Day17/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Day17;
+
+namespace Day17
+{
+    class Programm
+    {
+        [STAThread]
+        //Part 1
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+            //var input = File.ReadAllLines("Sample.txt");
+
+            var all = GetAllCombinations(input.ToList());
+            var count = 0;
+
+            foreach (var i in all)
+            {
+                var parts = i;
+
+                var sum = i.Split(',').Select(x => int.Parse(x)).Sum();
+
+                if (sum == 150)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+
+            }
+
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine(count);
+            Clipboard.SetText(count.ToString());
+            Console.ReadKey();
+        }
+
+        private static List<string> GetAllCombinations(List<string> list)
+        {
+            var anz = Enumerable.Range(1, list.Count);
+            var result = new List<string>();
+
+            foreach(var i in anz)
+            {
+                var x = list.ToArray().CombinationsWithoutRepetition(i).ToList();
+
+                result.AddRange(x.Select(y => string.Join(',', y)).ToList());
+            }
+
+            return result;
+        }
+    }
+}
