91813c4f27754e712afafa8f9439a836fe157216
Thomas Resch <Thomas.Resch@kcc.at>
2022-12-02 08:33:08 +0100

    2022/Day 2 - Part 2

--- 2022/Day2/Program.cs
+++ 2022/Day2/Program.cs
@@ -13,6 +13,58 @@ namespace Day1
     {
         [STAThread]
         //Part 1
+        //static void Main(string[] args)
+        //{
+        //    var input = File.ReadAllLines("Input.txt");
+        //    //var input = File.ReadAllLines("Sample.txt");
+
+        //    var sum = 0;
+
+        //    foreach(var line in input)
+        //    {
+        //        var part = line.Split(' ');
+
+        //        var left = part[0][0] - 'A';
+        //        var right = part[1][0] - 'X';
+
+        //        if (left == right)
+        //        {
+        //            sum += right + 1 + 3;
+        //        }
+        //        else if (left == 0 && right == 2)
+        //        {
+        //            sum += right + 1;
+        //        }
+        //        else if (left == 2 && right == 0)
+        //        {
+        //            sum += right + 1 + 6;
+        //        }
+        //        else
+        //        if (right > left)
+        //        {
+        //            sum += right + 1 + 6;
+        //        }
+        //        else
+        //        {
+        //            sum += right + 1;
+        //        }
+        //    }
+
+        //    // A X Rock             1 2 3    0 3 6
+        //    // B Y Paper
+        //    // C Z Scissors
+
+
+        //    Console.WriteLine("--------------------------------------");
+
+        //    var count = sum;
+
+        //    Console.WriteLine(count);
+        //    Clipboard.SetText(count.ToString());
+        //    Console.ReadKey();
+        //}
+
+        // Part 2
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("Input.txt");
@@ -20,12 +72,40 @@ namespace Day1
 
             var sum = 0;
 
-            foreach(var line in input)
+            foreach (var line in input)
             {
                 var part = line.Split(' ');
 
                 var left = part[0][0] - 'A';
-                var right = part[1][0] - 'X';
+                int right = -1;
+                var result = part[1][0];
+
+
+
+                switch (result)
+                {
+                    case 'X':
+                        right = left - 1;
+                        break;
+                    case 'Y':
+                        right = left;
+                        break;
+                    case 'Z':
+                        right = left + 1;
+                        break;
+                }
+
+
+                if (right < 0)
+                {
+                    right = 2;
+                }
+                else
+                if (right > 2)
+                {
+                    right = 0;
+                }
+
 
                 if (left == right)
                 {
