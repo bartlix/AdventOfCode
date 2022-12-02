958417217f470068452691477b29b3b4191ebbd4
Thomas Resch <Thomas.Resch@kcc.at>
2022-12-02 08:20:04 +0100

    2022 / Day 2 - Part 1

---
+++ 2022/Day2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Day1
+{
+    class Programm
+    {
+        [STAThread]
+        //Part 1
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+            //var input = File.ReadAllLines("Sample.txt");
+
+            var sum = 0;
+
+            foreach(var line in input)
+            {
+                var part = line.Split(' ');
+
+                var left = part[0][0] - 'A';
+                var right = part[1][0] - 'X';
+
+                if (left == right)
+                {
+                    sum += right + 1 + 3;
+                }
+                else if (left == 0 && right == 2)
+                {
+                    sum += right + 1;
+                }
+                else if (left == 2 && right == 0)
+                {
+                    sum += right + 1 + 6;
+                }
+                else
+                if (right > left)
+                {
+                    sum += right + 1 + 6;
+                }
+                else
+                {
+                    sum += right + 1;
+                }
+            }
+
+            // A X Rock             1 2 3    0 3 6
+            // B Y Paper
+            // C Z Scissors
+
+
+            Console.WriteLine("--------------------------------------");
+
+            var count = sum;
+
+            Console.WriteLine(count);
+            Clipboard.SetText(count.ToString());
+            Console.ReadKey();
+        }
+    }
+}
