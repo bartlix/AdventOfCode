013e3bee018a90206850c38e634fef7b31fee223
Thomas Resch <thomas.resch@kcc.at>
2022-12-02 22:00:16 +0100

    2015/Day19/Part 1

--- 2015/Day19/Program.cs
+++ 2015/Day19/Program.cs
@@ -20,10 +20,55 @@ namespace Day18
             var input = File.ReadAllLines("Input.txt");
             //var input = File.ReadAllLines("Sample.txt");
 
-            foreach(var)
+            var transParts = new List<(string key, string value)>();
 
-            //Console.WriteLine(result);
-            //Clipboard.SetText(result.ToString());
+            var action = false;
+
+            string baseTxt = "";
+
+            foreach (var line in input)
+            {
+                if (line == "")
+                {
+                    action = true;
+                    continue;
+                }
+
+                if (action)
+                {
+                    baseTxt = line;
+                    break;
+                }
+
+                var part = line.Split("=>");
+
+                transParts.Add((part[0].Trim(), part[1].Trim()));
+            }
+
+            var result = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var part in transParts)
+            {
+                var regex = new Regex(Regex.Escape(part.key));
+                var matches = regex.Matches(baseTxt);                
+
+                foreach (Match m in matches)
+                {
+                    sb.Append(baseTxt.Substring(0, m.Index));
+                    sb.Append(part.value);
+                    sb.Append(baseTxt.Substring(m.Index + m.Length));
+
+                    result.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+
+            var count = result.Distinct().Count();
+
+            Console.WriteLine(count);
+            Clipboard.SetText(count.ToString());
             Console.ReadKey();
         }
     }
