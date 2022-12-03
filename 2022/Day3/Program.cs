5eb44633df6b5e17c9cc925967d238dc8bfb3b28
Thomas Resch <thomas.resch@kcc.at>
2022-12-03 07:43:57 +0100

    2022/Day3/Part - 2

--- 2022/Day3/Program.cs
+++ 2022/Day3/Program.cs
@@ -14,6 +14,46 @@ namespace Day3
     {
         [STAThread]
         //Part 1
+        //static void Main(string[] args)
+        //{
+        //    var input = File.ReadAllLines("Input.txt");
+        //    //var input = File.ReadAllLines("Sample.txt");
+
+        //    var sum = 0;
+
+        //    foreach (var line in input)
+        //    {
+        //        var part1 = line.Substring(0, line.Length/2);
+        //        var part2 = line.Substring(line.Length/2);
+
+        //        foreach(var c in part1)
+        //        {
+        //            if (part2.Contains(c))
+        //            {
+        //                if (c <= 'Z')
+        //                {
+        //                    sum += (c - 'A' + 27);
+        //                    break;
+        //                }
+        //                else
+        //                {
+        //                    sum += (c - 'a' + 1);
+        //                    break;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    Console.WriteLine("--------------------------------------");
+
+        //    var count = sum;
+
+        //    Console.WriteLine(count);
+        //    Clipboard.SetText(count.ToString());
+        //    Console.ReadKey();
+        //}
+
+        //Part 2
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("Input.txt");
@@ -21,14 +61,11 @@ namespace Day3
 
             var sum = 0;
 
-            foreach (var line in input)
+            for (var i = 0; i < input.Length - 2; i+=3)
             {
-                var part1 = line.Substring(0, line.Length/2);
-                var part2 = line.Substring(line.Length/2);
-
-                foreach(var c in part1)
+                foreach (var c in input[i])
                 {
-                    if (part2.Contains(c))
+                    if (input[i+1].Contains(c) && input[i + 2].Contains(c))
                     {
                         if (c <= 'Z')
                         {
