8071da017e56c4952f816d0cbae5f583f04e080a
Thomas Resch <Thomas.Resch@kcc.at>
2022-12-06 07:41:23 +0100

    2022/Day6/Part 2

--- 2ß22/Day06/Program.cs
+++ 2ß22/Day06/Program.cs
@@ -9,22 +9,52 @@ namespace Day06
     {
         [STAThread]
         // Part 1
+        //static void Main(string[] args)
+        //{
+        //    var input = File.ReadAllText("Input.txt");
+        //    //var input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+
+        //    var count = 0;
+
+        //    for (var i = 3; i < input.Length; i++)
+        //    {
+        //        var search = input.Substring(i - 3, 4);
+
+        //        var x = search.GroupBy(x => x).Select(x => new { Key = x.Key, Items = x}).ToList();
+
+        //        var max = x.Max(x => x.Items.Count());
+
+        //        if(max == 1)
+        //        {
+        //            count = i + 1;
+        //            break;
+        //        }
+        //    }
+
+        //    Console.WriteLine("--------------------------------------");
+
+        //    Console.WriteLine(count);
+        //    Clipboard.SetText(count.ToString());
+        //    Console.ReadKey();
+        //}
+
+        // Part 2
         static void Main(string[] args)
         {
             var input = File.ReadAllText("Input.txt");
-            //var input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+            //var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 
             var count = 0;
-            
-            for (var i = 3; i < input.Length; i++)
+
+            for (var i = 13; i < input.Length; i++)
             {
-                var search = input.Substring(i - 3, 4);
-                
-                var x = search.GroupBy(x => x).Select(x => new { Key = x.Key, Items = x}).ToList();
+                var search = input.Substring(i - 13, 14);
+
+                var x = search.GroupBy(x => x).Select(x => new { Key = x.Key, Items = x }).ToList();
 
                 var max = x.Max(x => x.Items.Count());
 
-                if(max == 1)
+                if (max < 2)
                 {
                     count = i + 1;
                     break;
