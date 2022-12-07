9782fd3328dfa7fd17e2c67916bcae1786b46bae
Thomas Resch <Thomas.Resch@kcc.at>
2022-12-07 08:34:33 +0100

    2022/Day7/Part 1

---
+++ 2022/Day07/FileSystemInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    public abstract class FileSystemInfoBase
+    {
+        public string Name { get; set; }
+
+        public abstract int Size { get; }
+    }
+
+    public class DirectoryInfo : FileSystemInfoBase
+    {
+        public DirectoryInfo()
+        {
+            Content = new List<FileSystemInfoBase>();
+        }
+
+        public DirectoryInfo Parent { get; set; }
+
+        public List<FileSystemInfoBase> Content { get; set; }
+
+        public IEnumerable<FileSystemInfoBase> FlattenItems()
+        {
+            yield return this;
+            foreach (FileSystemInfoBase item in Content.OfType<DirectoryInfo>().SelectMany((DirectoryInfo d) => d.FlattenItems()))
+            {
+                yield return item;
+            }
+        }
+
+        public override int Size => Content.Sum(x => x.Size);
+    }
+
+    public class FileInfo : FileSystemInfoBase 
+    {
+        public FileInfo(string name, int size)
+        {
+            Name = name;
+            _size = size;
+        }
+
+        private int _size;
+
+        public override int Size => _size;        
+    }
+}
---
+++ 2022/Day07/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Day07
+{
+    internal class Programm
+    {
+        [STAThread]
+        // Part 1
+        static void Main(string[] args)
+        {
+            var content = File.ReadAllLines("Input.txt");
+            //var content = File.ReadAllLines("Sample.txt");
+
+            DirectoryInfo myTree = null;
+            DirectoryInfo currentPos = null;
+            var count = 0;
+
+            var regex = new Regex("\\d+");
+
+            var listContent = false;
+
+            foreach (var item in content)
+            {
+                if (item.StartsWith('$'))
+                {
+                    var command = item[1..].Trim().Split(' ');
+
+                    if (command[0] == "cd")
+                    {
+                        listContent = false;
+
+                        if (command[1] == "..")
+                        {
+                            currentPos = currentPos.Parent;
+                        }
+                        else if (command[1] == "/")
+                        {
+                            if (myTree == null)
+                            {
+                                myTree = new DirectoryInfo { Name = command[1] };
+                                currentPos = myTree;
+                            }
+                            else
+                            {
+                                currentPos = myTree;
+                            }
+                        }
+                        else
+                        {
+                            currentPos = currentPos.Content.OfType<DirectoryInfo>().Where(x => x.Name == command[1]).FirstOrDefault();
+                        }
+                    }
+                    else if (command[0] == "ls")
+                    {
+                        listContent = true;
+                        continue;
+                    }
+                }
+
+                if (listContent)
+                {
+                    var parts = item.Split(' ');
+                    if (parts[0] == "dir")
+                    {
+                        currentPos.Content.Add(new DirectoryInfo { Name = parts[1], Parent = currentPos });
+                    }
+                    else
+                    {
+                        currentPos.Content.Add(new FileInfo(parts[1], int.Parse(parts[0])));
+                    }
+                }
+            }
+
+            var bla = myTree.Content.OfType<DirectoryInfo>().SelectMany(x => x.FlattenItems()).Select(x => new { x.Name, x.Size }).ToList();
+
+            count = bla.Where(x => x.Size <= 100000).Sum(x => x.Size);
+
+            Console.WriteLine(count);
+            Clipboard.SetText(count.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
