94e7f667735bcc7115161dfe36123855d234f14a
Thomas Resch <thomas.resch@kcc.at>
2022-12-11 22:53:08 +0100

    c

---
+++ 2022/Day11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Day11
+{
+    internal class Programm
+    {
+        [STAThread]
+        // Part 1
+        static void Main(string[] args)
+        {
+            var content = File.ReadAllLines("Input.txt");
+            //var content = File.ReadAllLines("Sample.txt");
+            var sum = 0;
+
+            foreach (var line in content)
+            {
+
+            }
+
+            Console.WriteLine(sum);
+            Clipboard.SetText(sum.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
