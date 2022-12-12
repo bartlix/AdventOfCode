7f55135c76ca669abb284877a0118b18db375230
Thomas Resch <Thomas.Resch@kcc.at>
2022-12-12 15:26:35 +0100

    2022/Day11/Part 1

---
+++ 2022/Day11/MonkeyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    public class MonkeyInfo
+    {
+        public MonkeyInfo()
+        {
+            Items = new List<int>();
+        }
+        public int Id { get; set; }
+
+        public List<int> Items { get; set; }
+
+        public int Divisor { get; set; }
+
+        public int TrueId { get; set; }
+
+        public int FalseId { get; set; }
+
+        public string Operation { get; set; }
+        
+        public int InspectCount { get; set; }
+
+        public int GetWorryLevel(int value)
+        {
+            var help = Operation.Replace("old", value.ToString());
+
+            var result = Convert.ToInt32(new DataTable().Compute(help, null));
+
+            return result;
+        }
+    }
+}
--- 2022/Day11/Program.cs
+++ 2022/Day11/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Windows;
 
 namespace Day11
@@ -13,15 +14,105 @@ namespace Day11
             //var content = File.ReadAllLines("Sample.txt");
             var sum = 0;
 
+            var monkeys = new List<MonkeyInfo>();
+
+            MonkeyInfo currentMonkey = null;
+
             foreach (var line in content)
             {
+                if (line.StartsWith("Monkey"))
+                {
+                    var parts = line.Replace(":", "").Split(' ');
 
+                    currentMonkey = new MonkeyInfo { Id = int.Parse(parts[1]) };
+                }
+                else if (line.StartsWith("  Starting items:"))
+                {
+                    var parts = line.Substring(18).Split(',');
+                    foreach (var p in parts)
+                    {
+                        currentMonkey.Items.Add(int.Parse(p));
+                    }
+                }
+                else if (line.StartsWith("  Operation:"))
+                {
+                    var parts = line.Substring(13).Split("=");
+
+                    currentMonkey.Operation = parts[1].Trim();
+                }
+                else if (line.StartsWith("  Test:"))
+                {
+                    var parts = line.Substring(8).Split(' ');
+
+                    currentMonkey.Divisor = int.Parse(parts.Last());
+                }
+                else if (line.StartsWith("    If true:"))
+                {
+                    var parts = line.Substring(13).Split(' ');
+                    currentMonkey.TrueId = int.Parse(parts.Last());
+                }
+                else if (line.StartsWith("    If false:"))
+                {
+                    var parts = line.Substring(14).Split(' ');
+                    currentMonkey.FalseId = int.Parse(parts.Last());
+                }
+                else if (string.IsNullOrWhiteSpace(line))
+                {
+                    monkeys.Add(currentMonkey);
+                }
             }
+
+            monkeys.Add(currentMonkey);
+
+            for (var c = 0; c < 20; c++)
+            {
+                foreach (var m in monkeys)
+                {
+                    foreach (var item in m.Items.ToList())
+                    {
+                        m.InspectCount++;
+                        m.Items.Remove(item);
+
+                        var level = m.GetWorryLevel(item);
+
+                        var newWorryLevel = (level / 3);
+
+                        if (newWorryLevel % m.Divisor == 0)
+                        {
+                            var nextMonkey = monkeys.FirstOrDefault(x => x.Id == m.TrueId);
+                            if (nextMonkey != null)
+                            {
+                                nextMonkey.Items.Add(newWorryLevel);
+                            }
+                        }
+                        else
+                        {
+                            var nextMonkey = monkeys.FirstOrDefault(x => x.Id == m.FalseId);
+                            if (nextMonkey != null)
+                            {
+                                nextMonkey.Items.Add(newWorryLevel);
+                            }
+                        }
+                    }
+                }
+            }
+
+            var ordered = monkeys.OrderByDescending(x => x.InspectCount).ToList();
+
+            sum = ordered[0].InspectCount * ordered[1].InspectCount;
 
             Console.WriteLine(sum);
             Clipboard.SetText(sum.ToString());
 
             Console.ReadKey();
+        }
+
+        private static void Display(List<MonkeyInfo> monkeys)
+        {
+            foreach(var m in monkeys)
+            {
+                Console.WriteLine($"Monkey {m.Id}: {string.Join(',', m.Items)}");
+            }
         }
     }
 }
